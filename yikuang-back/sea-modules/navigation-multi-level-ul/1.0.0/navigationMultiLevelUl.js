c5e396a4266dc827f06c78269f24a276ac85ff36
mazhiwen <81245995@qq.com>
2016-07-04 13:36:54 +0800

    后台系统第一次上传

---
+++ yikuang-back/sea-modules/navigation-multi-level-ul/1.0.0/navigationMultiLevelUl.js
@@ -0,0 +1,69 @@
+define(function(require,exports,module) {
+	
+	function navigationMultiLevelUl(outerId,hrefArray,directoryObj){
+
+		//var hrefArray=hrefArray;
+		//var directoryObj=directoryObj;
+		//var outerId=outerId;
+		var navigationHtmlText='<ul id="navigation">';
+
+		var hrefArrayInd=0;
+
+		function iterationGenerateHtml(array,fatherDirIndex){
+			
+
+			//var fatherDirIndex=fatherDirIndex;
+
+			$.each(array,function(index,value){
+
+				console.log(fatherDirIndex+"_"+index);
+				if(value.constructor==Array) {
+					if(value[1].length>0){
+						navigationHtmlText+="<li><div>"+value[0]+"</div><ul>";
+
+						iterationGenerateHtml(value[1],fatherDirIndex+"_"+index);
+						navigationHtmlText+="</ul></li>";
+					}else{
+						
+						navigationHtmlText+="<li><a href='"+hrefArray[hrefArrayInd]+"?di="+fatherDirIndex+"_"+index+"'>"+value[0]+"</a></li>";hrefArrayInd++;
+					}
+				}else{
+					navigationHtmlText+="<li><a href='"+hrefArray[hrefArrayInd]+"?di="+fatherDirIndex+"_"+index+"'>"+value+"</a></li>";hrefArrayInd++;
+				}		
+			});
+			
+		}
+		
+		iterationGenerateHtml(directoryObj,'');
+		navigationHtmlText+='</ul>';
+		
+		$(outerId).append(navigationHtmlText);
+
+		$("#navigation div").click(function(){
+			$(this).next().css("display")=="block"?$(this).next().hide():$(this).next().show();				
+		});
+
+		//$("#navigation a").each(function(index,ele){
+		//	$(this).attr("href",hrefArray[index]);
+		//});
+		
+		
+		
+
+	}
+
+
+
+	module.exports=navigationMultiLevelUl;
+
+
+
+	navigationMultiLevelUl.prototype._init=function(){
+
+	}
+
+
+
+	
+});
+
---
+++ yikuang-back/static/index-content/src/indexContent.js
@@ -0,0 +1,162 @@
+define(function(require) {
+	
+	$ = require('jquery');
+
+	
+	var hrefArray=[];
+	hrefArray[0]='index-content.html';
+	hrefArray[1]='';
+	hrefArray[2]='2222';
+	hrefArray[3]='4';
+	hrefArray[4]='5';
+	hrefArray[5]='6';
+	hrefArray[6]='7';
+	hrefArray[7]='8';
+	hrefArray[8]='9';
+	hrefArray[9]='10';
+	hrefArray[10]='11';
+	hrefArray[11]='12';
+	hrefArray[12]='13';
+	hrefArray[13]='';
+	hrefArray[14]='';
+	hrefArray[15]='';
+	hrefArray[16]='';
+	hrefArray[17]='';
+	hrefArray[18]='';
+
+
+
+	var directoryObj=[
+			[
+				"内容管理",
+				[
+					[
+						"文章管理",
+						["文章分类","添加编辑分类","文章列表","添加/编辑文章"]
+					],
+					[
+						"资讯管理",
+						["资讯列表","新闻抓取列表","上传/编辑早报","上传/编辑要闻"]
+					],
+					[
+						"专题管理",
+						["专题列表","新建/编辑专题"]
+					],
+					[
+						"话题管理",
+						["话题列表","新增/编辑话题"]
+					],
+					[
+						"匿名八卦管理",
+						[]
+					],
+					[
+						"Q&A管理（尚未收到，待定）",
+						[]
+					]
+				]
+			],
+			[
+				"广告位管理",
+				[
+					[
+						"广告列表",
+						[]
+					],
+					[
+						"添加/编辑广告",
+						[]
+					]
+				]
+			],
+			[
+				"评论管理",
+				[
+					[
+						"评论列表",
+						[]
+					],
+					[
+						"评论详情",
+						[]
+					]
+				]
+			],
+			[
+				"推送管理",
+				[
+					[
+						"系统消息推送列表",
+						[]
+					],
+					[
+						"新增/编辑系统推送",
+						[]
+					],
+					[
+						"APP内消息推送",
+						[]
+					],
+					[
+						"新增/编辑APP内推送",
+						[]
+					]
+				]
+			],
+			[
+				"用户管理",
+				[
+					[
+						"用户列表",
+						[]
+					],
+					[
+						"添加/编辑用户",
+						[]
+					],
+					[
+						"用户反馈列表",
+						[]
+					],
+					[
+						"反馈详情",
+						[]
+					]
+				]
+			],
+			[
+				"权限管理",
+				[
+					[
+						"角色管理",
+						[]
+					],
+					[
+						"添加/编辑角色",
+						[]
+					],
+					[
+						"管理员列表",
+						[]
+					],
+					[
+						"添加/编辑管理员",
+						[]
+					]
+				]
+			]
+		];
+
+
+	var navigationMultiLevelUl = require('navigationMultiLevelUl');
+	var my_navigationMultiLevelUl = new navigationMultiLevelUl("#left-content",hrefArray,directoryObj);
+	
+
+
+
+
+	
+
+	
+});
+
