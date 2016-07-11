64990d7d9c50efcf6d74b25c531ec7f3a1440ee8
mazhiwen <81245995@qq.com>
2016-07-11 14:20:18 +0800

    上传文件库

--- develop/bs/js/main/subject-edit/1.0.0/index.js
+++ develop/bs/js/main/subject-edit/1.0.0/index.js
@@ -11,5 +11,29 @@ define(function(require) {
 
 		console.log(checkedArticleArray);
 	});
+
+
+
+
+	var coverImageDom=document.getElementById('cover-image');
+	coverImageDom.addEventListener("change",function(event){
+		var files=coverImageDom.files;
+		var formData=new FormData();
+		for(var i=0,file;file=files[i];i++){
+			formData.append(file.name, file);
+		}
+		var xhr = new XMLHttpRequest();
+		xhr.addEventListener('load',function(event){
+			JSON.parse(xhr.responseText,function(key,value){
+
+				console.log(value);
+
+			});
+
+
+		});
+		xhr.open("POST", "../js/main/subject-edit/1.0.0/upload.php", true);
+		xhr.send(formData);
+	},false);
 });
 
