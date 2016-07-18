5ebfc395423c47b6fea7eb18b638b340d0587ad5
mazhiwen <81245995@qq.com>
2016-07-18 10:24:38 +0800

    后台接口 以及其他调优

--- develop/bs/js/main/subject_list/1.0.0/index.js
+++ develop/bs/js/main/subject_list/1.0.0/index.js
@@ -3,7 +3,7 @@ seajs.use("../js/modules/commonnavigation/1.0.0/commonNavigation");
 
 
 define(function(require) {
-	/*	
+		
 	var $=require('jquery');
 
 	$.ajax({
@@ -13,14 +13,26 @@ define(function(require) {
 		data:{page:1,size:10,token:12},
 		dataType:"json",
 		success:function(data){
-			var subject_list_container=$("#subject_list_container");
+			var subject_list_tbody=$("#subject_list_tbody");
 
-			subject_list_container.empty();
+			subject_list_tbody.empty();
 			
 			$.each(data['data']['result'],function(key,value){
 				
 				//console.log(value);
-				subject_list_container.append('<tr><td>'+value['id']+'</td><td>'+value['name']+'</td><td>'+value['id']+'</td><td>'+value['id']+'</td><td>'+value['id']+'</td><td>'+value['id']+'</td><td></td><td><a href="#">编辑</a> <a href="#">删除</a></td></tr>');
+				var s='<tr><td>'+value['id']+'</td><td>'+value['name']+'</td><td>'+value['id']+'</td><td>'+value['id']+'</td><td>'+value['id']+'</td>';
+				if(value['name']){
+					s+='<td><input type="checkbox"></td>';
+				}else{
+					s+='<td><input type="checkbox"></td>';
+				}
+				if(value['name']){
+					s+='<td><input type="checkbox"></td>';
+				}else{
+					s+='<td><input type="checkbox"></td>';
+				}
+				s+='<td><a href="">编辑</a> <a href="">删除</a></td></tr>'; 
+				subject_list_tbody.append(s);
 
 			});
 
@@ -30,10 +42,6 @@ define(function(require) {
 
 
 	});
-*/
-
-
-
 
 
 	var paging = require('paging');
