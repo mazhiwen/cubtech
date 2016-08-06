f24b70cf40b965bd7a859c770157f6147bb3bac6
SDX <1756093825@qq.com>
2016-08-06 19:06:31 +0800

    article

--- develop/h5/js/main/article.js
+++ develop/h5/js/main/article.js
@@ -1,7 +1,11 @@
 var pageController = {
     init: function () {
-
+        var articleId = eq.getUrlString('id');
+        proHub.article.getArticleDetail(articleId,function(data){
+            $("#detail").html(data.data.result.content)
+        })
     }
+
 }
 
 $(function () {
